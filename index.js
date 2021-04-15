c9e38137babada9398b23d815edcc99005acdb08
shmuelsimpser <ssimpser@gmail.com>
2021-04-14 21:49:22 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(n) {
+    return n += 1
+}
+function decrement(n) {
+    return n -= 1
+}
+function makeInt(word) {
+    return parseInt(word, 10) 
+}
+function preserveDecimal(words) {
+    return parseFloat(words)
+}
